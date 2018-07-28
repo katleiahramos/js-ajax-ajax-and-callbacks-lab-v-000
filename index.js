68d7d85fe6954375ad7cacc4e052f71a0c5f28d1
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-28 20:22:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function displayResult(result) {
      <h3>name: ${result.full_name}</h3>
      <p>url: ${result.html_url}</p>
      <p>owner: ${result.owner.login}</p>
-     <a href="#" data-url="${result.html_url}" onclick="showCommits();return false;">See Commits</a>
+     <p><a href="#" data-url="${result.html_url}" onclick="showCommits(this);return false;">See Commits</a></p>
   </div>`)
 }
 
