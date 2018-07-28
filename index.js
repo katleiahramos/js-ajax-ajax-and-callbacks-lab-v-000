b428bb2c52a5761267518887c93e46cfb6a53da4
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-28 19:23:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ $(document).ready(function (){
 
 });
 
+function displayError() {
+  $('#errors').html("There was an error. Please try again.")
+}
 
 function searchRepositories() {
   // queries the GitHub repository search API
   const searchTerms = $('#searchTerms').val()
 
-}
-
-function displayError() {
-  $('#errors').html("There was an error. Please try again.")
 }
