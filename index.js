f1b6c9ed50b4c5e4e29e06173f1abb9db5d677ca
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-28 19:15:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ $(document).ready(function (){
 
 function searchRepositories() {
   // queries the GitHub repository search API
-  window.$('#searchTerms').val('tetris')
   const searchTerms = $('#searchTerms').val()
   debugger
 }
