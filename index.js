509484d202d84e10c678d15e9d9b147e3185e691
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-28 20:53:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function displayResult(result) {
 
 function showCommits(el) {
 
-  $.get(`${el.dataset.url}/commits`, commits => renderCommits(commits))
+  $.get(https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, commits => renderCommits(commits))
 }
 
 function renderCommits(commits) {
