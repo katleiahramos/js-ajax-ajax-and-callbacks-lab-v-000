4a0813425ffb1c8efa5d9f3d6d78f54f06915d48
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-28 20:37:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,6 +45,6 @@ function renderCommits(commits) {
 function renderCommit(commit) {
   debugger
   return(
-    `<li>${commit.sha} - ${commit.author.login} - <img src="${commit.author.avatar_url} width="100px" height="100px""></li>`
+    `<li>${commit.sha} - ${commit.author.login} - <img src="${commit.author.avatar_url}" width="100px" height="100px"></li>`
   )
 }
